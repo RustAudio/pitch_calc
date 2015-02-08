27c22e9a4c754b3a680d618bbcab0e019c80992f
mitchmindtree <mitchell.nordine@gmail.com>
2015-02-09 01:13:56 +1100

    Updated to latest rustc

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,9 @@
 //!  Created by Mitchell Nordine at 11:26PM on November 02, 2014.
 //!
 
-#![feature(core, rand)]
+#![feature(core)]
 
+extern crate rand;
 extern crate "rustc-serialize" as rustc_serialize;
 
 pub use self::calc::{
