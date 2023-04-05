87ca2b367c1a9f3f38903d30b069d568308ff18b
Josh Hansen <hansen.joshuaa@gmail.com>
2023-04-05 15:28:49 -0700

    Remove redundant parens

--- src/utils.rs
+++ src/utils.rs
@@ -4,7 +4,7 @@ use num_traits::PrimInt as Int;
 #[inline]
 pub fn modulo<I: Int>(a: I, b: I) -> I {
     match a % b {
-        r if (r > I::zero() && b < I::zero()) || (r < I::zero() && b > I::zero()) => (r + b),
+        r if (r > I::zero() && b < I::zero()) || (r < I::zero() && b > I::zero()) => r + b,
         r => r,
     }
 }
